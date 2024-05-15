1d357f872bc5a1d3e49edafe016b1a43e92ab4a8
bijoymamud <bijoymamud.09@gmail.com>
2024-05-15 16:43:08 +0600

    delete the user info

--- delete.php
+++ delete.php
@@ -1,3 +1,12 @@
 <?php
 
-echo"this is delete";
+include("connection.php");
+
+$id = $_GET["id"];
+
+$deletequery = "DELETE FROM `jobregistration` WHERE id=$id";
+$query = mysqli_query($con, $deletequery);
+header("location:displaytable.php");
+
+
+?>
